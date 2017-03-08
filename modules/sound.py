404d8a63087cf67a1ebf3df5f920b07281f487f5
rkalvait <rkalvait@emich.edu>
2017-03-08 03:47:22 +0000

    Changed the audio device to 1,0

--- modules/sound.py
+++ modules/sound.py
@@ -4,7 +4,7 @@ sample_time = 1
 
 while True:
 
-    command = "/usr/bin/arecord -D plughw:0,0 -d " + str(sample_time) + " -f S16_LE | /usr/bin/sox -t .wav - -n stat"
+    command = "/usr/bin/arecord -D plughw:1,0 -d " + str(sample_time) + " -f S16_LE | /usr/bin/sox -t .wav - -n stat"
 
     p = subprocess.Popen(command, bufsize=1, shell=True,  stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 
